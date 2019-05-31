d45407a5f5bc12de2ab057a44bedcb1da4e50c52
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-31 16:48:43 +0200

    added some new test using __construct with default variables and custom variables

--- src/Entity/Dinosaur.php
+++ src/Entity/Dinosaur.php
@@ -3,9 +3,19 @@
 namespace App\Entity;
 
 
+
 class Dinosaur
 {
     private $length = 0;
+    private $genus;
+    private $isCarnivorous;
+
+    public function __construct(string $genus = 'Unknown', bool $isCarnivorous = false)
+    {
+
+        $this->genus = $genus;
+        $this->isCarnivorous = $isCarnivorous;
+    }
 
     public function getLength(): int
     {
@@ -15,5 +25,14 @@ class Dinosaur
     public function setLength(int $length)
     {
         $this->length = $length;
+    }
+
+    public function getDescription(): string
+    {
+        return sprintf(
+            'The %s %scarnivorous dinosaur is %d meters long',
+            $this->genus,
+            $this->isCarnivorous ? '' : 'non-',
+            $this->length);
     }
 }
