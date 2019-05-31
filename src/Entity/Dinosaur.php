a828f0f6ee8b79242311b9e86a7361c44d76c823
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2019-05-31 12:00:09 +0200

    try first test in TDD way

---
+++ src/Entity/Dinosaur.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Entity;
+
+
+class Dinosaur
+{
+    private $length = 0;
+
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
+}
